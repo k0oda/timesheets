e07c615be7873ee7a59f69566b4da6829df78d64
kukree <kulevgen32@gmail.com>
2019-08-17 20:54:49 +0600

    add: login view

---
+++ authentication/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+from .forms import LoginForm
+
+# Create your views here.
+def login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['login']
+            password = form.cleaned_data['password']
+
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                return redirect('/')
+            else:
+                return redirect('/login/')  # TODO: Add authentication error page
+    else:
+        form = LoginForm()
+    return render(request, 'authentication/login.html', {'form': form})
