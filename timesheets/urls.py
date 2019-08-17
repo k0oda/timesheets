71ebeec47f25a0f884e40fc41a695ca4925cf6f8
kukree <kulevgen32@gmail.com>
2019-08-17 20:55:03 +0600

    add: login url

--- timesheets/urls.py
+++ timesheets/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from authentication.views import login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', login),
 ]
