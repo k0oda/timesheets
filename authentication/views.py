e0e3967ae9911f3edc1050a76de436ed9239025f
kukree <kulevgen32@gmail.com>
2019-08-18 21:36:02 +0600

    add: register view

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate
-from .forms import LoginForm
+from django.contrib.auth.models import User
+from .forms import LoginForm, RegisterForm
 
 
 class Authentication:
@@ -20,3 +21,32 @@ class Authentication:
         else:
             form = LoginForm()
         return render(request, 'authentication/login.html', {'form': form})
+
+    @staticmethod
+    def register(request):
+        if request.method == 'POST':
+            form = RegisterForm(request.POST)
+            if form.is_valid():
+                login = form.cleaned_data['login']
+                email = form.cleaned_data['email']
+                first_name = form.cleaned_data['first_name']
+                last_name = form.cleaned_data['last_name']
+                password = form.cleaned_data['password']
+                repeat_password = form.cleaned_data['repeat_password']
+
+                if password == repeat_password:
+                    user = User.objects.create_user(
+                        login, 
+                        email=email, 
+                        password=password, 
+                        first_name=first_name, 
+                        last_name=last_name
+                        )
+                    user.save()
+                    return redirect('/')
+                else:
+                    return redirect('/login/')  # TODO: Add registration error page
+        else:
+            form = RegisterForm()
+        return render(request, 'authentication/register.html', {'form': form})
+
