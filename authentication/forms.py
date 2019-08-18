9353e1f9924b3f96be7f72047de7355ab036936a
kukree <kulevgen32@gmail.com>
2019-08-18 21:35:44 +0600

    add: RegisterForm

--- authentication/forms.py
+++ authentication/forms.py
@@ -11,3 +11,36 @@ class LoginForm(forms.Form):
     password.widget.attrs.update({
         'class': 'form-control',
     })
+
+class RegisterForm(forms.Form):
+    login = forms.CharField(min_length=3, max_length=20)
+    email = forms.EmailField()
+    first_name = forms.CharField(min_length=3, max_length=50)
+    last_name = forms.CharField(min_length=3, max_length=50)
+    password = forms.CharField(widget=forms.PasswordInput)
+    repeat_password = forms.CharField(widget=forms.PasswordInput)
+
+    login.widget.attrs.update({
+        'class': 'form-control',
+    })
+
+    email.widget.attrs.update({
+        'class': 'form-control',
+    })
+
+    first_name.widget.attrs.update({
+        'class': 'form-control',
+    })
+
+    last_name.widget.attrs.update({
+        'class': 'form-control',
+    })
+
+    password.widget.attrs.update({
+        'class': 'form-control',
+    })
+
+    repeat_password.widget.attrs.update({
+        'class': 'form-control',
+    })
+
