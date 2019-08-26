95092463e35d55156bf611a1af8ce93a9c4cb9e0
kukree <kulevgen32@gmail.com>
2019-08-27 03:50:22 +0600

    add: attaching user to current session in login view

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from .forms import LoginForm, RegisterForm
 
@@ -15,6 +15,7 @@ class Authentication:
 
                 user = authenticate(username=username, password=password)
                 if user is not None:
+                    login(request, user)
                     return redirect('/')
                 else:
                     return redirect('/login/')  # TODO: Add authentication error page
