06b58f069ab2e9d7f8ea1de71912ebb24bc9c3f0
kukree <kulevgen32@gmail.com>
2019-08-29 17:08:53 +0600

    add: unauthenticated user index page, and authentication checking in main view

--- timesheets_app/views.py
+++ timesheets_app/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 class Main:
     @staticmethod
     def main(request):
-        return render(request, 'timesheets/index.html')
+        if request.user.is_authenticated:
+            return render(request, 'timesheets/index.html')
+        else:
+            return render(request, 'timesheets/unauthenticated_index.html')
