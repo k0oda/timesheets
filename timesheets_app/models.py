b59579266873620fdbe1e4e8a68e79862ce6c50b
kukree <kulevgen32@gmail.com>
2019-08-29 23:00:44 +0600

    add: Company and Employees models

--- timesheets_app/models.py
+++ timesheets_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    name = models.CharField(max_length=150)
+
+
+class Employees(models.Model):
+    company_id = models.BigIntegerField()
+    user_id = models.BigIntegerField()
