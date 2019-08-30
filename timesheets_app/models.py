10b241b51e6bdbedaece5cb342a75e149917cfcd
kukree <kulevgen32@gmail.com>
2019-08-31 02:16:33 +0600

    remove: Employees model from timesheets_app

--- timesheets_app/models.py
+++ timesheets_app/models.py
@@ -4,7 +4,5 @@ from django.db import models
 class Company(models.Model):
     name = models.CharField(max_length=150)
 
-
-class Employees(models.Model):
-    company_id = models.BigIntegerField()
-    user_id = models.BigIntegerField()
+    def __str__(self):
+        return self.name
--- timesheets_app/views.py
+++ timesheets_app/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from timesheets_app.models import Company, Employees
+from timesheets_app.models import Company
+from authentication.models import UserProfile
 
 
 class Main:
     @staticmethod
     def main(request):
         if request.user.is_authenticated:
-            companies = Employees.objects.filter(user_id=request.user.id)
-            return render(request, 'timesheets/index.html', context={'companies': companies})
+            company_id = UserProfile.objects.get(username=request.user.username).company_id
+            company = Company.objects.get(pk=company_id)
+            return render(request, 'timesheets/index.html', context={'company': company})
         else:
             return render(request, 'timesheets/unauthenticated_index.html')
