e4d25f19aea8af44e95ed11fc3de83a09ef260ce
kukree <kulevgen32@gmail.com>
2019-08-31 01:26:24 +0600

    add: user's companies output in timesheets_app views

--- timesheets_app/views.py
+++ timesheets_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from timesheets_app.models import Company, Employees
 
 
 class Main:
     @staticmethod
     def main(request):
         if request.user.is_authenticated:
-            return render(request, 'timesheets/index.html')
+            companies = Employees.objects.filter(user_id=request.user.id)
+            return render(request, 'timesheets/index.html', context={'companies': companies})
         else:
             return render(request, 'timesheets/unauthenticated_index.html')
