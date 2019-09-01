25c8f877aa80401729c29d1527581fc5d2a3f704
kukree <kulevgen32@gmail.com>
2019-09-01 22:43:22 +0600

    add: checking if user in company in index template and main view

--- timesheets_app/views.py
+++ timesheets_app/views.py
@@ -8,7 +8,10 @@ class Main:
     def main(request):
         if request.user.is_authenticated:
             company_id = UserProfile.objects.get(username=request.user.username).company_id
-            company = Company.objects.get(pk=company_id)
+            if Company.objects.filter(pk=company_id).exists():
+                company = Company.objects.get(pk=company_id)
+            else:
+                company = 0
             return render(request, 'timesheets/index.html', context={'company': company})
         else:
             return render(request, 'timesheets/unauthenticated_index.html')
