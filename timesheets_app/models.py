fa023d9772b0dde1912ac5cf0d34f0725efcac18
kukree <kulevgen32@gmail.com>
2019-09-19 18:55:09 +0600

    add: Task, Client, Project models, change id fields in Entry model to ForeignKeys

--- timesheets_app/models.py
+++ timesheets_app/models.py
@@ -9,12 +9,35 @@ class Company(models.Model):
     def __str__(self):
         return self.name
 
+class Task(models.Model):
+    name = models.CharField(max_length=150)
+    company_id = models.BigIntegerField()
+    default_hourly_rate = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return self.name
+
+class Client(models.Model):
+    name = models.CharField(max_length=150)
+    email = models.EmailField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Project(models.Model):
+    name = models.CharField(max_length=150)
+    client = models.ForeignKey(Client, models.CASCADE)
+    tasks = models.ManyToManyField(Task)
+
+    def __str__(self):
+        return self.name
+
 class Entry(models.Model):
     date = models.DateField(default=date.today())
-    project_id = models.BigIntegerField()
-    task_id = models.BigIntegerField()
+    project = models.ForeignKey(Project, models.CASCADE, default=None)
+    task = models.ForeignKey(Task, models.CASCADE, default=None)
     notes = models.CharField(max_length=350, blank=True)
     timer = models.TimeField(default='0:00')
 
     def __str__(self):
-        return self.name
+        return self.project.name
