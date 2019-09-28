96c5e0e594f2495d716e046220cfab77d9e85cb6
kukree <kulevgen32@gmail.com>
2019-09-28 14:00:27 +0600

    add: delete_entry to urls

--- timesheets/urls.py
+++ timesheets/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('time/<int:year>/<int:month>/<int:day>/', CompanyPanel.time, name='time'),
     path('time/add/<int:year>/<int:month>/<int:day>/', CompanyPanel.add_entry, name='add_entry'),
     path('time/edit/<int:pk>/', CompanyPanel.edit_entry, name='edit_entry'),
+    path('time/delete/<int:pk>/', CompanyPanel.delete_entry, name="delete_entry"),
     path('expenses/', CompanyPanel.expenses, name='expenses'),
     path('projects/', CompanyPanel.projects, name='projects'),
     path('team/', CompanyPanel.team, name='team'),
