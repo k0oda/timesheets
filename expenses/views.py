0d02fdd0fda183418cb49444e6e0b269cfd862bd
kukree <kulevgen32@gmail.com>
2019-10-03 18:40:14 +0600

    aad: getting expenses list in view

--- expenses/views.py
+++ expenses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from company_panel.models import Company
+from expenses.models import Expense
 
 
 class Expenses:
@@ -12,5 +13,10 @@ class Expenses:
             company = Company.objects.get(pk=company_id)
         else:
             company = 0
-        return render(request, 'timesheets/company_panel/expenses.html', context={'company': company})
 
+        expenses = Expense.objects.filter(company=company).order_by('-date')
+
+        return render(request, 'timesheets/company_panel/expenses.html', context={
+            'company': company,
+            'expenses': expenses
+        })
