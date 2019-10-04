81ef4041a3d8e2c15548e28ca4c285e475fdbac8
kukree <kulevgen32@gmail.com>
2019-10-04 20:05:42 +0600

    add: delete_expense url

--- timesheets/urls.py
+++ timesheets/urls.py
@@ -42,6 +42,7 @@ urlpatterns = [
     path('expenses/', Expenses.expenses, name='expenses'),
     path('expenses/add/', Expenses.add_expense, name='add_expense'),
     path('expenses/edit/<int:expense_id>/', Expenses.edit_expense, name='edit_expense'),
+    path('expenses/delete/<int:expense_id>/', Expenses.delete_expense, name='delete_expense'),
     path('projects/', Projects.projects, name='projects'),
     path('team/', Team.team, name='team'),
     path('invoices/', Invoices.invoices, name='invoices'),
