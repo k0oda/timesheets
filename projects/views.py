04c2fe216dec9f818b69e1706d307c5c4a2f8417
kukree <kulevgen32@gmail.com>
2019-10-10 21:31:54 +0600

    add: Project, Client, Task objects to template context

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from company_panel.models import Company
+from projects.models import Project
+from manage_app.models import Client, Task
 
 
 class Projects:
@@ -12,5 +14,14 @@ class Projects:
             company = Company.objects.get(pk=company_id)
         else:
             company = 0
-        return render(request, 'timesheets/company_panel/projects.html', context={'company': company})
+        
+        projects = Project.objects.filter(company=company)
+        clients = Client.objects.filter(company=company)
+        tasks = Task.objects.filter(company=company)
+        return render(request, 'timesheets/company_panel/projects.html', context={
+            'company': company,
+            'projects': projects,
+            'clients': clients,
+            'tasks': tasks
+        })
 
