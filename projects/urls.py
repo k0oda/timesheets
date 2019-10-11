1efb63f301397fc2aa22c919aa59abe56bec96a1
kukree <kulevgen32@gmail.com>
2019-10-11 20:50:38 +0600

    add: delete_project to urls

--- projects/urls.py
+++ projects/urls.py
@@ -4,5 +4,6 @@ from projects.views import Projects
 urlpatterns = [
     path('', Projects.projects, name='projects'),
     path('add/', Projects.add_project, name='add_project'),
-    path('edit/<int:pk>', Projects.edit_project, name='edit_project')
+    path('edit/<int:pk>', Projects.edit_project, name='edit_project'),
+    path('delete/<int:pk>', Projects.delete_project, name='delete_project')
 ]
