c2768cc0059087597bb7502b3ebdf8b1e290ba43
kukree <kulevgen32@gmail.com>
2019-10-11 20:47:18 +0600

    add: edit_project to urls

--- projects/urls.py
+++ projects/urls.py
@@ -3,5 +3,6 @@ from projects.views import Projects
 
 urlpatterns = [
     path('', Projects.projects, name='projects'),
-    path('add/', Projects.add_project, name='add_project')
+    path('add/', Projects.add_project, name='add_project'),
+    path('edit/<int:pk>', Projects.edit_project, name='edit_project')
 ]
