a548041e518409f3af9951b72b6fa217c9ccd98e
kukree <kulevgen32@gmail.com>
2019-10-14 20:31:26 +0600

    add: tasks and expense_categories views to urls

--- manage_app/urls.py
+++ manage_app/urls.py
@@ -3,5 +3,7 @@ from manage_app.views import Manage
 
 urlpatterns = [
     path('', Manage.base, name='manage'),
-    path('clients', Manage.clients, name='clients')
+    path('clients', Manage.clients, name='clients'),
+    path('tasks', Manage.tasks, name='tasks'),
+    path('expense_categories', Manage.expense_categories, name='expense_categories')
 ]
