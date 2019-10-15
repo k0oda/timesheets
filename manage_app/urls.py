b7cbc50325f35c74dfdd80deffbcdcbaf74a96f2
kukree <kulevgen32@gmail.com>
2019-10-15 20:59:11 +0600

    add: delete_client to urls, add url to Delete Client button in Edit Client modal in clients.html

--- manage_app/urls.py
+++ manage_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('clients', Manage.clients, name='clients'),
     path('clients/new/', Manage.add_client, name='add_client'),
     path('clients/edit/<int:pk>/', Manage.edit_client, name='edit_client'),
+    path('clients/delete/<int:pk>/', Manage.delete_client, name='delete_client'),
     path('tasks', Manage.tasks, name='tasks'),
     path('expense_categories', Manage.expense_categories, name='expense_categories')
 ]
