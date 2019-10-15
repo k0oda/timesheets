fbfeddb893f51e97ef967e2ba618cbab98c01e29
kukree <kulevgen32@gmail.com>
2019-10-15 20:55:30 +0600

    add: edit_client view to urls

--- manage_app/urls.py
+++ manage_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', Manage.base, name='manage'),
     path('clients', Manage.clients, name='clients'),
     path('clients/new/', Manage.add_client, name='add_client'),
+    path('clients/edit/<int:pk>/', Manage.edit_client, name='edit_client'),
     path('tasks', Manage.tasks, name='tasks'),
     path('expense_categories', Manage.expense_categories, name='expense_categories')
 ]
