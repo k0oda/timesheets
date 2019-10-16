2e984b77925a70419aa1dbc4c5e438587b6c05c8
kukree <kulevgen32@gmail.com>
2019-10-16 20:22:27 +0600

    add: tasks to template context in tasks view

--- manage_app/views.py
+++ manage_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from company_panel.models import Company
-from manage_app.models import Client
+from manage_app.models import Client, Task
 
 
 class Manage:
@@ -67,7 +67,12 @@ class Manage:
             company = Company.objects.get(pk=company_id)
         else:
             company = 0
-        return render(request, 'manage/tasks.html', context={'company': company})
+        tasks = Task.objects.filter(company=company)
+
+        return render(request, 'manage/tasks.html', context={
+            'company': company,
+            'tasks': tasks
+        })
 
     @staticmethod
     @login_required
