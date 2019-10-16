45c969bc38cd81d442da583ca9f16118ecc302c1
kukree <kulevgen32@gmail.com>
2019-10-16 20:36:07 +0600

    add: delete_task view to urls

--- manage_app/urls.py
+++ manage_app/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('tasks', Manage.tasks, name='tasks'),
     path('tasks/new/', Manage.add_task, name='add_task'),
     path('tasks/edit/<int:pk>/', Manage.edit_task, name='edit_task'),
+    path('tasks/delete/<int:pk>/', Manage.delete_task, name='delete_task'),
     path('expense_categories', Manage.expense_categories, name='expense_categories')
 ]
