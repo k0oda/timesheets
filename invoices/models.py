74dbcf57041d0750c6ab0555985bbb97deaaec3b
kukree <kulevgen32@gmail.com>
2019-10-25 18:36:33 +0600

    Add Item and Invoice models to invoices app

--- invoices/models.py
+++ invoices/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from manage_app.models import Client
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=350, blank=True)
+    amount = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+class Invoice(models.Model):
+    client = models.ForeignKey(Client, models.CASCADE)
+    date = models.DateField(auto_now=True)
+    items = models.ManyToManyField(Item)
+    notes = models.CharField(max_length=350, blank=True)
+
+    def __str__(self):
+        return self.client
