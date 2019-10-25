eb2ec732a69076791abb238eb345f8c045eb5b9e
kukree <kulevgen32@gmail.com>
2019-10-25 18:40:14 +0600

    Add company field to Item and Invoice models

--- invoices/models.py
+++ invoices/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from manage_app.models import Client
+from company_panel.models import Company
 
 
 class Item(models.Model):
+    company = models.ForeignKey(Company, models.CASCADE)
     name = models.CharField(max_length=150)
     description = models.CharField(max_length=350, blank=True)
     amount = models.FloatField()
@@ -13,6 +15,7 @@ class Item(models.Model):
 
 class Invoice(models.Model):
     client = models.ForeignKey(Client, models.CASCADE)
+    company = models.ForeignKey(Company, models.CASCADE)
     date = models.DateField(auto_now=True)
     items = models.ManyToManyField(Item)
     notes = models.CharField(max_length=350, blank=True)
