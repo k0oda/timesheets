f3cfee000315e2fabbe885c9fc2f8b608bbd91eb
kukree <kulevgen32@gmail.com>
2019-10-26 21:12:46 +0600

    Move Invoice object to top of models.py, add amount and unit_price fields in Item model
    Add default values for Item model fields

--- invoices/models.py
+++ invoices/models.py
@@ -3,23 +3,27 @@ from manage_app.models import Client
 from company_panel.models import Company
 
 
-class Item(models.Model):
-    company = models.ForeignKey(Company, models.CASCADE)
-    name = models.CharField(max_length=150)
-    description = models.CharField(max_length=350, blank=True)
-    amount = models.FloatField()
-
-    def __str__(self):
-        return self.name
-
-
 class Invoice(models.Model):
     client = models.ForeignKey(Client, models.CASCADE)
     company = models.ForeignKey(Company, models.CASCADE)
     date = models.DateField(auto_now=True)
-    items = models.ManyToManyField(Item)
-    total_amount = models.FloatField()
+    total_amount = models.FloatField(null=True)
     notes = models.CharField(max_length=350, blank=True)
 
     def __str__(self):
         return self.client.name
+
+    def get_items(self):
+        return Item.objects.filter(invoice=self)
+
+
+class Item(models.Model):
+    company = models.ForeignKey(Company, models.CASCADE)
+    invoice = models.ForeignKey(Invoice, models.CASCADE)
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=350, blank=True)
+    unit_price = models.FloatField(default=0.0)
+    amount = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
