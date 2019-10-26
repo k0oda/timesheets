53e9abe598e2e43dc427f8283bd985df9fc840fa
kukree <kulevgen32@gmail.com>
2019-10-26 21:14:06 +0600

    Add add_invoice view, add it to urls

--- invoices/urls.py
+++ invoices/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from invoices.views import Invoices
 
 urlpatterns = [
-    path('', Invoices.invoices, name='invoices')
+    path('', Invoices.invoices, name='invoices'),
+    path('new/', Invoices.add_invoice, name='add_invoice'),
 ]
--- invoices/views.py
+++ invoices/views.py
@@ -22,3 +22,34 @@ class Invoices:
             'invoices': invoices,
             'items': items
         })
+
+    @staticmethod
+    @login_required
+    def add_invoice(request):
+        company_id = request.user.company_id
+        if Company.objects.filter(pk=company_id).exists():
+            company = Company.objects.get(pk=company_id)
+        else:
+            company = 0
+
+        if request.method.lower() == 'post':
+            client = Client.objects.get(name=request.POST.get('client'), company=company)
+            date = request.POST.get('date')
+            notes = request.POST.get('notes')
+            new_invoice = Invoice.objects.create(
+                company=company,
+                client=client,
+                date=date,
+                notes=notes
+            )
+            new_invoice.save()
+            return redirect('add_item', new_invoice.pk)
+        else:
+            clients = Client.objects.filter(company=company)
+
+            return render(request, 'invoices/new_invoice.html', context={
+                'company': company,
+                'clients': clients
+            })
+
+    @staticmethod
