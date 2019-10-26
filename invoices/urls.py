31bdd04a53c4fdde470746b63827aa69997697c8
kukree <kulevgen32@gmail.com>
2019-10-27 01:07:32 +0600

    Add edit_item to urls, add url to Edit Item modal window's form

--- invoices/urls.py
+++ invoices/urls.py
@@ -4,5 +4,6 @@ from invoices.views import Invoices
 urlpatterns = [
     path('', Invoices.invoices, name='invoices'),
     path('new/', Invoices.add_invoice, name='add_invoice'),
-    path('new/items/<int:invoice_pk>', Invoices.add_item, name='add_item')
+    path('new/items/<int:invoice_pk>', Invoices.add_item, name='add_item'),
+    path('edit/items/<int:pk>/', Invoices.edit_item, name='edit_item')
 ]
