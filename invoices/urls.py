bc8de02f3b4e15bc8e345bff4668a5934a1129c9
kukree <kulevgen32@gmail.com>
2019-10-27 20:31:50 +0600

    Add Edit invoice modal window in template, add edit_invoice view to urls

--- invoices/urls.py
+++ invoices/urls.py
@@ -4,6 +4,7 @@ from invoices.views import Invoices
 urlpatterns = [
     path('', Invoices.invoices, name='invoices'),
     path('new/', Invoices.add_invoice, name='add_invoice'),
+    path('edit/<int:pk>/', Invoices.edit_invoice, name='edit_invoice'),
     path('new/items/<int:invoice_pk>', Invoices.add_item, name='add_item'),
     path('edit/items/<int:pk>/', Invoices.edit_item, name='edit_item'),
     path('delete/items/<int:pk>/', Invoices.delete_item, name='delete_item')
