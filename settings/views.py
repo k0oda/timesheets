72798d829de860c097542a8e909aa71105c0c852
kukree <kulevgen32@gmail.com>
2019-10-29 18:33:18 +0600

    Add Settings.main view

--- settings/views.py
+++ settings/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from company_panel.models import Company
 
-# Create your views here.
+
+class Settings:
+    @staticmethod
+    @login_required
+    def main(request):
+        company_id = request.user.company_id
+        if Company.objects.filter(pk=company_id).exists():
+            company = Company.objects.get(pk=company_id)
+        else:
+            company = -1
+
+        return render(request, 'settings/settings.html', context={
+            'company': company
+        })
