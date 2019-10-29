7d97e6dea827cd9cbd07170dc5452ee40b0f2273
kukree <kulevgen32@gmail.com>
2019-10-29 19:50:48 +0600

    Create edit_user view

--- settings/urls.py
+++ settings/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from settings.views import Settings
 
 urlpatterns = [
-    path('', Settings.main, name='settings')
+    path('', Settings.main, name='settings'),
+    path('user/', Settings.edit_user, name='edit_user')
 ]
--- settings/views.py
+++ settings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from company_panel.models import Company
 
@@ -16,3 +16,14 @@ class Settings:
         return render(request, 'settings/settings.html', context={
             'company': company
         })
+
+    @staticmethod
+    @login_required
+    def edit_user(request):
+        if request.method.lower() == 'post':
+            request.user.username = request.POST.get('username')
+            request.user.first_name = request.POST.get('first_name')
+            request.user.last_name = request.POST.get('last_name')
+            request.user.email = request.POST.get('email')
+            request.user.save()
+        return redirect('settings')
