4703f78fcf42a7a05143f1c3c9fc52c951b3bcd6
kukree <kulevgen32@gmail.com>
2019-10-30 23:35:02 +0600

    Add attrs for CreateCompanyForm

--- company_panel/forms.py
+++ company_panel/forms.py
@@ -3,6 +3,10 @@ from .models import Company
 
 
 class CreateCompanyForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(CreateCompanyForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Company name'})
+    
     class Meta:
         model = Company
         fields = ('name',)
