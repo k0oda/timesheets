e34d97436675d0fc61e47b888a85e181bfca39fa
kukree <kulevgen32@gmail.com>
2019-11-25 19:06:15 +0600

    Add detailed_project_info_access check in Projects project view and projects.html

--- projects/views.py
+++ projects/views.py
@@ -23,21 +23,24 @@ class Projects:
     @staticmethod
     @login_required
     def project(request, pk):
-        project = Project.objects.get(company=request.user.company, pk=pk)
-        team = get_user_model().objects.filter(company=request.user.company)
-        totals = {}
-        for user in team:
-            entries = Entry.objects.filter(company=request.user.company, user=user, project=project)
-            if entries:
-                totals[user.username] = time(0, 0)
-                for entry in entries:
+        if request.user.role.detailed_project_info_access:
+            project = Project.objects.get(company=request.user.company, pk=pk)
+            team = get_user_model().objects.filter(company=request.user.company)
+            totals = {}
+            for user in team:
+                entries = Entry.objects.filter(company=request.user.company, user=user, project=project)
+                if entries:
+                    totals[user.username] = time(0, 0)
+                    for entry in entries:
                     timer_delta = timedelta(hours=entry.timer.hour, minutes=entry.timer.minute)
-                    current_total = totals[user.username]
-                    totals[user.username] = (datetime.min + (timedelta(hours=current_total.hour, minutes=current_total.minute) + timer_delta)).time()
-        return render(request, 'projects/project.html', context={
-            'project': project,
-            'totals': totals
-        })
+                        current_total = totals[user.username]
+                        totals[user.username] = (datetime.min + (timedelta(hours=current_total.hour, minutes=current_total.minute) + timer_delta)).time()
+            return render(request, 'projects/project.html', context={
+                'project': project,
+                'totals': totals
+            })
+        else:
+            return redirect('projects')
 
     @staticmethod
     @login_required
