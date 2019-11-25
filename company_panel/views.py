ceb53a127d24a3f849c3359752f1c1a87d3f2124
kukree <kulevgen32@gmail.com>
2019-11-25 21:07:48 +0600

    Add add_role view

--- company_panel/views.py
+++ company_panel/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from company_panel.models import Company
+from company_panel.models import Company, Role
 from company_panel.forms import CreateCompanyForm
 
 
@@ -27,3 +27,27 @@ class CompanyPanel:
         else:
             form = CreateCompanyForm()
         return render(request, 'company_panel/new_company.html', {'form': form})
+    
+    @staticmethod
+    @login_required
+    def add_role(request):
+        if request.user.role.manage_roles_access:
+            if request.method.lower() == 'post':
+                role = Role.objects.create(
+                    company=request.user.company,
+                    name=request.POST.get('name'),
+                    user_info_access=request.POST.get('user_info_access'),
+                    detailed_project_info_access=request.POST.get('detailed_project_info_access'),
+                    project_manage_access=request.POST.get('project_manage_access'),
+                    invite_user_access=request.POST.get('invite_user_access'),
+                    kick_user_access=request.POST.get('kick_user_access'),
+                    expenses_manage_access=request.POST.get('expenses_manage_access'),
+                    invoices_manage_access=request.POST.get('invoices_manage_access'),
+                    client_manage_access=request.POST.get('client_manage_access'),
+                    task_manage_access=request.POST.get('task_manage_access'),
+                    expense_category_manage_access=request.POST.get('expense_category_manage_access'),
+                    edit_company_info_access=request.POST.get('edit_company_info_access'),
+                    manage_roles_access=request.POST.get('manage_roles_access')
+                )
+                role.save()
+        return redirect('settings')
--- settings/urls.py
+++ settings/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from settings.views import Settings
+from company_panel.views import CompanyPanel
 
 urlpatterns = [
     path('', Settings.main, name='settings'),
     path('user/', Settings.edit_user, name='edit_user'),
     path('user/password/', Settings.change_password, name='change_password'),
     path('company/', Settings.edit_company, name='edit_company'),
-    path('company/leave/', Settings.leave_company, name='leave_company')
+    path('company/leave/', Settings.leave_company, name='leave_company'),
+    path('role/add/', CompanyPanel.add_role, name='add_role')
 ]
