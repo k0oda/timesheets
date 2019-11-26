e4e14defde5f154654aed78e5d7f66bf13a5c560
kukree <kulevgen32@gmail.com>
2019-11-26 19:57:03 +0600

    Create edit_role and delete_role views

--- company_panel/urls.py
+++ company_panel/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('company/edit/', CompanyPanel.edit_company, name='edit_company'),
     path('company/leave/', CompanyPanel.leave_company, name='leave_company'),
     path('company/new', CompanyPanel.new_company, name='new_company'),
-    path('role/add/', CompanyPanel.add_role, name='add_role')
+    path('role/add/', CompanyPanel.add_role, name='add_role'),
+    path('role/edit/<int:pk>/', CompanyPanel.edit_role, name='edit_role'),
+    path('role/delete/<int:pk>/', CompanyPanel.delete_role, name='delete_role')
 ]
