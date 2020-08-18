ff743894f2aa1983cd95e742b432ec86c12eed2e
kukree <kulevgen32@gmail.com>
2020-08-19 05:24:54 +0600

    Remove unnecessary fields from Invoice model, add necessary import to models file

--- invoices/models.py
+++ invoices/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from manage_app.models import Client
 from company_panel.models import Company
+from projects.models import Project
 
 
 class Invoice(models.Model):
@@ -12,8 +13,6 @@ class Invoice(models.Model):
     client = models.ForeignKey(Client, models.CASCADE, related_name='+')
     company = models.ForeignKey(Company, models.CASCADE, related_name='+')
     date = models.DateField()
-    total_amount = models.IntegerField(null=True, default=0)
-    total_unit_price = models.DecimalField(null=True, default=0, max_digits=20, decimal_places=2)
     total_price = models.DecimalField(null=True, default=0, max_digits=20, decimal_places=2)
     notes = models.TextField(max_length=350, blank=True)
 
