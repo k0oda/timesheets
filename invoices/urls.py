b956f52ab76797a0e8879332547050d4daf397ff
kukree <kulevgen32@gmail.com>
2020-08-19 05:25:22 +0600

    Add invoice approving and PDF file output

--- invoices/urls.py
+++ invoices/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.invoices, name='invoices'),
     path('new/', views.add_invoice, name='add_invoice'),
     path('edit/<int:pk>/', views.edit_invoice, name='edit_invoice'),
+    path('approve/<int:pk>/', views.approve_invoice, name='approve_invoice'),
     path('delete/<int:pk>/', views.delete_invoice, name='delete_invoice'),
     path('new/items/<int:invoice_pk>', views.add_item, name='add_item'),
     path('edit/items/<int:pk>/', views.edit_item, name='edit_item'),
--- invoices/views.py
+++ invoices/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.conf import settings
+from django.http import FileResponse
 from invoices.models import Invoice, Item
 from manage_app.models import Client
 from invoices.forms import CreateInvoice, CreateItem
+
+from io import BytesIO
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.enums import TA_JUSTIFY
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.units import inch
 
 @login_required
 def invoices(request):
@@ -51,6 +59,80 @@ def edit_invoice(request, pk):
             invoice.date = new_invoice.date
             invoice.notes = new_invoice.notes
             invoice.save()
+    return redirect('invoices')
+
+@login_required
+def approve_invoice(request, pk):
+    if request.user.role.invoices_manage_access:
+        invoice = get_object_or_404(Invoice, company=request.user.company, pk=pk)
+
+        buffer = BytesIO()
+        doc = SimpleDocTemplate(buffer, pagesize=letter, rightMargin=72, leftMargin=72, topMargin=72, bottomMargin=18)
+        
+        styles = getSampleStyleSheet()
+        styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
+
+        story = []
+
+        text = request.user.company.name
+        story.append(Paragraph(text, styles['Heading2']))
+
+        text = str(invoice.date)
+        story.append(Paragraph(text, styles['Normal']))
+
+        text = f'To {invoice.client}'
+        story.append(Paragraph(text, styles['Normal']))
+
+        text = invoice.client.address
+        story.append(Paragraph(text, styles['Normal']))
+        story.append(Spacer(1, 12))
+
+        text = 'Bills:'
+        story.append(Paragraph(text, styles['Heading3']))
+
+        employees = {}
+        total_bill = 0
+        for item in invoice.items.all():
+            employees_objects = []
+            for entry in item.project.entries.all():
+                if entry.user not in employees_objects:
+                    employees[entry.user.username] = {
+                        'name': f'{entry.user.first_name} {entry.user.last_name}',
+                        'total_hours': entry.timer.hour,
+                        'bill': entry.timer.hour * entry.user.hourly_rate,
+                    }
+                    total_bill += entry.timer.hour * entry.user.hourly_rate
+                    employees_objects.append(entry.user)
+                else:
+                    employees[entry.user.username]['total_hours'] += entry.timer.hour
+                    employees[entry.user.username]['bill'] += entry.timer.hour * entry.user.hourly_rate
+                    total_bill += entry.timer.hour * entry.user.hourly_rate
+
+        for employee in employees.values():
+            text = employee['name']
+            story.append(Paragraph(text, styles['Heading5']))
+
+            text = f'Total hours: {employee["total_hours"]}'
+            story.append(Paragraph(text, styles['Normal']))
+
+            text = f'Bill: {employee["bill"]}'
+            story.append(Paragraph(text, styles['Normal']))
+            story.append(Spacer(1, 12))
+
+        text = f'Total bill: {total_bill}'
+        story.append(Paragraph(text, styles['Heading3']))
+        story.append(Spacer(1, 12))
+
+        doc.build(story)
+        buffer.seek(0)
+
+        for item in invoice.items.all():
+            for entry in item.project.entries.all():
+                entry.delete()
+            item.delete()
+        invoice.delete()
+
+        return FileResponse(buffer, as_attachment=True, filename=f'{invoice.client}_{invoice.date}.pdf')
     return redirect('invoices')
 
 @login_required
